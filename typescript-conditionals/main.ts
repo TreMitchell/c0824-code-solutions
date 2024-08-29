f70631a94d76fe6c01238817c477da1f9dad947f
TreMitchell <trem963@gmail.com>
2024-08-29 05:23:54 +0000

    Complete typescript-conditionals exercise.

---
+++ typescript-conditionals/main.ts
@@ -0,0 +1,130 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+
+/*  */
+function isUnderFive(number: number): boolean {
+  const isLessThanFive: boolean = number < 5;
+  return isLessThanFive;
+}
+/* The log method of the console object is being called by two arguments, a string
+& the numbers being called by the variable isUnderFive. */
+console.log('Is 4 < 5?:', isUnderFive(4));
+console.log('Is 9 < 5?:', isUnderFive(9));
+
+/*  */
+function isEven(number: number): boolean {
+  const isNumberEven: boolean = number % 2 === 0;
+  return isNumberEven;
+}
+/* The log method of the console object is being called by two arguments, a string
+& the numbers being called by the variable isEven. */
+console.log('Is 4 divisible by 2?:', isEven(4));
+console.log('Is 9 divisible by 2?:', isEven(9));
+
+/*  */
+function startsWithJ(string: string): boolean {
+  const characterStartWithJ: boolean = string[0] === 'J';
+  return characterStartWithJ;
+}
+/* The log method of the console object is being called by two arguments, a string
+& the strings being called by the variable startsWithJ. */
+console.log('Does JavaScript start with J?:', startsWithJ('JavaScript'));
+console.log('Does CSS start with J?:', startsWithJ('CSS'));
+
+/*  */
+interface Person {
+  name?: string;
+  age: number;
+}
+/*  */
+function isOldEnoughToDrink(person: Person): boolean {
+  const isLegalToDrink: boolean = person.age >= 21;
+  return isLegalToDrink;
+}
+/* The commands are in the console */
+
+/*  */
+function isOldEnoughToDrive(person: Person): boolean {
+  const isLegalToDrive: boolean = person.age >= 16;
+  return isLegalToDrive;
+} /* The commands are in the console */
+
+/*  */
+function isOldEnoughToDrinkAndDrive(person: Person): boolean {
+  const isLegalToDrink: boolean = person.age >= 21;
+  const isLegalToDrive: boolean = person.age >= 16;
+  if (isLegalToDrink && isLegalToDrive) {
+    return false;
+  }
+  return false;
+}
+
+/*
+There is an if statement with a conditional expression checking if ph is less than 0 or pH > 14 and an opening curly brace for the code block. */
+function categorizeAcidity(pH: number): string {
+  if (pH < 0 || pH > 14) {
+    return 'Invalid pH level';
+  } else if (pH === 7) {
+    return 'neutral';
+  } else if (pH > 7) {
+    return 'base';
+  } else {
+    return 'acid';
+  }
+}
+/* The log method of the console object is being called by two arguments, a string &
+the number being called by the variable categorizeAcidity. */
+console.log('acidic level:', categorizeAcidity(12));
+
+/*
+There is a switch statement with a conditional expression evaluating the variable name.
+There is a case clause checking for the strings yakko, wakko, and dot. */
+function introduceWarnerBro(name: string): string {
+  switch (name.toLowerCase()) {
+    case 'yakko':
+    case 'wakko':
+      return "We're the warner brothers!";
+    case 'dot':
+      return "I'm cute";
+    default:
+      return 'Goodnight everybody!';
+  }
+}
+/* The log method of the console object is being called by one arguments, the string
+being called by the variable name. */
+console.log(introduceWarnerBro('yakko'));
+
+/*
+There is a switch statement with a conditional expression evaluating the variable name.
+There is a case clause checking for the strings action, comedy, horror, etc... . */
+function recommendMovie(genre: string): string {
+  switch (genre.toLowerCase()) {
+    case 'action':
+      return 'Die Hard';
+    case 'comedy':
+      return 'Ted';
+    case 'horror':
+      return 'It';
+    case 'drama':
+      return 'Fall Guy';
+    case 'musical':
+      return 'Moana';
+    case 'sci-fi':
+      return 'Godzilla Minus One';
+    default:
+      return 'Genre not recognized. Choose between action, comedy, horror, drama, musical, or sci-fi';
+  }
+}
+/* The log method of the console object is being called by one arguments, the string
+being called by the variable recommendMovie. */
+console.log(recommendMovie('action'));
+console.log(recommendMovie('comedy'));
+console.log(recommendMovie('romance'));
---
+++ typescript-conditionals/typescript-conditionals-quiz-notes.md
@@ -0,0 +1,52 @@
+# typescript-conditionals-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- Give 6 examples of comparison operators.
+  - Equal (==), greater than or equal to (>=), less than or equal to (<=), not equal (!=), (strict not equal) !==, and greater than (>).
+- What data type do comparison expressions evaluate to?
+  - Booleans are the data type that comparison expressions evaluate.
+- What is the purpose of an `if` statement?
+  - The purpose of an if statement is to have a conditional expression to make the condition either true or false.
+- Is `else` required in order to use an `if` statement?
+  - Else isn;t required in order to use an if statement.
+- Describe the syntax (structure) of an `if` statement.
+  - Beginning with the if statement, inside the parentheses is an expression so there is a condition that it checks for. Then a conditional expression will evaluate to true or false.
+- What are the three logical operators?
+  - Logical AND (&&), Logical OR (||) and Logical NOT (!).
+- How do you compare two different expressions in the same condition?
+  - You can use an else-if statement to compare two different expressions in the same direction.
+- What is the purpose of a `switch` statement?
+  - The purpose of a switch statement is to be more readable and user-friendly version of an else-if statement. Also, switch statements are useful for a collection of conditions or cases that needs to be checked.
+- Is a `default` clause required in order to use a `switch` statement?
+  - The default clause is optional for a switch statement..
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
