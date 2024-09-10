8a8ffdce0b8ece1f28847c13015d3b0d4b51a9bd
TreMitchell <trem963@gmail.com>
2024-09-10 17:37:25 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('Hello, world');
+const $element = document.querySelector('h1');
+console.log('$element', $element);
+console.dir($element);
+const $explain = document.querySelector('#explanation');
+console.log('$explain', $explain);
+console.dir($explain);
+const $clue = document.querySelector('.hint');
+console.log('$clue', $clue);
+console.dir($clue);
+const $matches = document.querySelectorAll('p');
+console.log($matches);
+const $links = document.querySelectorAll('.example-link');
+console.log($links);
