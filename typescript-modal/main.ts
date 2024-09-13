8a4f14038a098d29a334ac8f2881d6ef22ba652b
TreMitchell <trem963@gmail.com>
2024-09-13 16:46:36 +0000

    Completed typescript-modal exercise.

---
+++ typescript-modal/main.ts
@@ -0,0 +1,15 @@
+const $openModal = document.querySelector('.open-modal');
+const $dismissModal = document.querySelector('.dismiss-modal');
+const $dialog = document.querySelector('dialog');
+
+if (!$openModal) throw new Error('.open-modal Query failed!');
+if (!$dismissModal) throw new Error('.dismiss-modal Query failed!');
+if (!$dialog) throw new Error('dialog Query failed!');
+
+$openModal.addEventListener('click', () => {
+  $dialog.showModal();
+});
+
+$dismissModal.addEventListener('click', () => {
+  $dialog.close();
+});
